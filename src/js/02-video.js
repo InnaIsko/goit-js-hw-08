235e542f40d9d085835759aebf49e097f20e8a12
Inna <isko.inna.94@gmail.com>
2022-10-11 19:06:41 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  const time = data.seconds;
+  localStorage.setItem(LOCALSTORAGE_KEY, time);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+const localStorageValue = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player.setCurrentTime(localStorageValue);
