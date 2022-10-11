2c71f72b56f1c9826ae3851bace23fd65d120571
Inna <isko.inna.94@gmail.com>
2022-10-11 16:06:47 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
+const ulGalleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryList(galleryItems);
+ulGalleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryList(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item"  onclick="event.preventDefault()" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`;
+    })
+    .join('');
+}
+console.log(createGalleryList(galleryItems));
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox', function () {
+  // Do something…
+});
+
+gallery.on('error.simplelightbox', function (e) {});
 console.log(galleryItems);
